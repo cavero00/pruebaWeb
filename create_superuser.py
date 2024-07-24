878a7a4b4ed1aa83ac9cecfb390670e0f128c181
cavero00 <burvullon@gmail.com>
2024-07-24 22:22:59 +0200

    fix errors

--- create_superuser.py
+++ create_superuser.py
@@ -4,7 +4,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
 django.setup()
 
 User = get_user_model()
